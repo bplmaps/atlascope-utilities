141676dddd6f07bcf23638d909e944a89af005f4
Garrett Dash Nelson <garrettdashnelson@gmail.com>
2020-07-20 17:38:43 -0400

    beginning new workflow

---
+++ new-workflow/2-mask.py
@@ -0,0 +1,41 @@
+import os
+import gdal
+
+
+
+# use os.walk() generates lists of root, directory, and files
+for r, d, f in os.walk('./spatial_imagery'):
+
+
+	# loop through every file in the spatial_imagery directory
+	# note that this does not filter for only *.tif images
+	# if there are other non-tif images in that directory, they'll get tested too
+	for file in f:
+
+		basename = file.split('.')[0]
+
+		warpOptions = gdal.WarpOptions(
+		format = 'GTiff',
+		cutlineDSName = './footprint/Boundary.geojson',
+		cutlineLayer = 'Boundary',
+		cutlineWhere = "identifier='{}'".format(basename),
+		cropToCutline = True,
+		copyMetadata = True,
+		dstAlpha = True,
+		multithread = True
+		)
+
+
+		print('🤿 Beginning mask of {}'.format(basename))
+
+		ds = gdal.Warp('./masked/{}-masked.tif'.format(basename),'./spatial_imagery/{}'.format(file),
+		options = warpOptions
+		)
+
+		del ds
+
+		print('✅ Done')
+
+
+
+

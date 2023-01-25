6bd7d281a8a00b9960b6649e8e38932d2a04a1b3
Ian Spangler <39773984+itspangler@users.noreply.github.com>
2023-01-25 14:36:43 -0500

    archived old workflow + download inputs func
    - moved atlascope v1 workflows to an `archives` folder
    - added allmaps annotation requests in download inputs function

---
+++ modern-workflow/README.md
@@ -0,0 +1,20 @@
+# Geotransforming urban atlases using Allmaps and GDAL
+
+the python script in this repo will do:
+
+![atlascopify](./media/atlascopify.png)
+
+## Usage
+
+1. clone this repo and `cd` into it
+2. create symbolic link to `atlascopify.py`
+
+```sh
+sudo ln -s pwd usr/local/bin
+```
+
+3. execute with
+
+```sh
+atlascopify.py --identifier <commonwealth:id>
+```
--- modern-workflow/atlascopify.py
+++ modern-workflow/atlascopify.py
@@ -3,6 +3,7 @@
 import argparse
 import requests
 import os
+import json
 from os import path
 
 
@@ -14,43 +15,72 @@ parser.add_argument('--identifier', type=str,
 
 args = parser.parse_args()
 
-
+# 
 
 def downloadInputs(identifier):
-    
-    # ask allmaps API what the Allmaps ID is for the Commonwealth Manifest ID we sent over
-    # allMapsAPIRequest = requests.get(f'https://id.allmaps.org/?url=https://collections.leventhalmap.org/search/{identifier}/manifest.json')
-    # print(allMapsAPIRequest.text)
 
-    # once we have the Allmaps Manifest ID, do these things ...
-    # use the Allmaps API to get all the Map IDs from that Manifest
-    # download each JSON annotation for each of those Map IDs
-
-
-    # here we download the TIFFs from Digital Commonwealth
+# here we download the TIFFs from Digital Commonwealth
 
     digitalCommonwealthManifestRequest = requests.get(f'https://collections.leventhalmap.org/search/{identifier}/manifest')
     manifestJSON = digitalCommonwealthManifestRequest.json()
 
     counter = 0
 
+    # use digital commonwealth API to get all map images associated with a manifest/commonwealth ID
+
     for sequence in manifestJSON['sequences']:
         for canvas in sequence['canvases']:
             for image in canvas['images']:
-                imageURL = image['resource']['service']['@id']
-                tiffURL = f'{imageURL}/full/full/0/default.tif'
+                if counter > 4:
+                    break
+                else:
+                    imageURL = image['resource']['service']['@id']
+                    tiffURL = f'{imageURL}/full/full/0/default.tif'
 
-                print(f'⤵️ Downloading image {tiffURL}')
+                    print(f'⤵️ Downloading image {tiffURL}')
 
-                imageRequest = requests.get(tiffURL, stream=True)
+                    #  download the image for each atlas plate
 
-                # right now we're naming them with a ordinal counter, fix this later
-                with open(f'./tmp/img/{counter}.tif', 'wb') as fd:
-                    for chunk in imageRequest.iter_content(chunk_size=128):
-                        fd.write(chunk)
+                    imageRequest = requests.get(tiffURL, stream=True)
 
-                counter = counter+1
+                    # right now we're naming them with a ordinal counter, fix this later
 
+                    with open(f'./tmp/img/{counter}.tif', 'wb') as fd:
+                        for chunk in imageRequest.iter_content(chunk_size=128):
+                            fd.write(chunk)
+
+                    counter = counter+1
+
+    print("✅ All images downloaded!")
+# here we download the allmaps georeference annotations for each plate inside an atlas
+
+    # ask allmaps API what the Allmaps ID is for the Commonwealth Manifest ID we sent over
+
+    allmapsAPIRequest = requests.get(f'https://annotations.allmaps.org/?url=https://www.digitalcommonwealth.org/search/{identifier}/manifest.json')
+    allmapsManifest = allmapsAPIRequest.json()
+    
+    counter = 0
+
+    # use the Allmaps API to get all the Map IDs from that Manifest
+
+    for item in allmapsManifest['items']:
+        if counter > 4:
+            break
+        allmapsMapID = item['id']
+        mapURL = f'https://annotations.allmaps.org/maps/{allmapsMapID}'
+        print(f'⤵️ Downloading annotation {mapURL}')
+            
+        # download each JSON annotation for each of those Map IDs
+
+        annoRequest = requests.get(mapURL, stream=True)
+        allmapsAnnotation = annoRequest.json()
+
+        with open(f'./tmp/annotations/{counter}.json', 'w') as f:
+            json.dump(allmapsAnnotation, f)
+
+        counter = counter+1
+    
+    print("✅ All annotations downloaded!")
 
 def createDirectoryStructure():
     if not os.path.exists('./tmp'):
